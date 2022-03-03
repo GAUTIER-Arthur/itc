c4c5bcc7e23ad3ead7109c1f3a7b40654551b394
Arthur <arthur.64.gautier@gmail.com>
2022-03-03 15:23:02 +0100

    Adding Parametrized Divide test

--- src/test/java/CalculTest2.java
+++ src/test/java/CalculTest2.java
@@ -81,5 +81,28 @@ class CalculsTest2 {
 	        String n = null;
 	        assertNull(n);
 	}
+	
+	//Données divide
+		static Stream<Arguments> testValueDivide() throws Throwable 
+		{
+		    return Stream.of(
+		        Arguments.of(2,2,1), // appellera : testMultiplier(2,2,4)
+		        Arguments.of(6,6,1),
+		        Arguments.of(3,2,1.5)
+		    );
+		}
+		
+		@ParameterizedTest(name="Division numéro {index}: nombre1={0}, nombre2={1}, résultat attendu = {2}")
+		@MethodSource("testValueDivide")
+		void testDiviser(int firstNumber, int secondNumber, int expectedResult) 
+		{
+			// Partie paramétrée
+		        Calculs monCal = new Calculs(firstNumber, secondNumber);
+		        assertEquals(expectedResult, monCal.diviser(), "test en échec pour " + firstNumber + " * " + secondNumber + " != " + expectedResult); 
+		
+		    // Partie indépendante (les paramètres peuvent ne servir qu'à une sous partie des tests)
+		        String n = null;
+		        assertNull(n);
+		}
 
 }
